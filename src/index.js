1c5926aaf9398df87dc2c78f903ada1c35f9054a
Fernando Dias <fernando.dias@ise.org.br>
2025-06-17 18:16:53 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,108 @@
+const player1 = {
+  NOME: "Mario",
+  VELOCIDADE: 4,
+  MANOBRABILIDADE: 3,
+  PODER: 3,
+  PONTOS: 0,
+};
+
+const player2 = {
+  NOME: "Luigi",
+  VELOCIDADE: 3,
+  MANOBRABILIDADE: 4,
+  PODER: 4,
+  PONTOS: 0,
+};
+
+// Função para calcular o resultado da partida
+async function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+async function playRaceEngine(character1, character2) {
+  for (let round = 1; round <= 5; round++) {
+    console.log(`🏁 Rodada ${round}`);
+
+    // sortear bloco
+    let block = await getRandomBlock();
+    console.log(`Bloco sorteado: ${block}`);
+
+    // rolar dados
+    let diceResult1 = await rollDice();
+    let diceResult2 = await rollDice();
+
+    let totalTestSkill1 = 0;
+    let totalTestSkill2 = 0;
+
+    if (block === "RETA") {
+      totalTestSkill1 = diceResult1 + character1.VELOCIDADE;
+      totalTestSkill2 = diceResult2 + character1.VELOCIDADE;
+
+      await logRollResult(
+        character1.NOME,
+        "velocidade",
+        diceResult1,
+        character1.VELOCIDADE
+      );
+      await logRollResult(
+        character2.NOME,
+        "velocidade",
+        diceResult2,
+        character2.VELOCIDADE
+      );
+    }
+
+    if (block === "CURVA") {
+      totalTestSkill1 = diceResult1 + character1.MANOBRABILIDADE;
+      totalTestSkill2 = diceResult2 + character1.MANOBRABILIDADE;
+
+      await logRollResult(
+        character1.NOME,
+        "manobrabilidade",
+        diceResult1,
+        character1.MANOBRABILIDADE
+      );
+      await logRollResult(
+        character2.NOME,
+        "manobrabilidade",
+        diceResult2,
+        character2.MANOBRABILIDADE
+      );
+    }
+
+    if (block === "CONFRONTO") {
+      totalTestSkill1 = diceResult1 + character1.PODER;
+      totalTestSkill2 = diceResult2 + character1.PODER;
+    }
+  }
+}
+
+async function logRollResult(characterName, block, diceResult, attribute) {
+  console.log(`${characterName} 🎲 rolou um dado de ${block} ${diceResult}`);
+}
+
+async function getRandomBlock() {
+  let random = Math.random();
+  let result;
+
+  switch (true) {
+    case random < 0.33:
+      result = "RETA";
+      break;
+    case random < 0.66:
+      result = "CURVA";
+      break;
+    default:
+      result = "CONFRONTO";
+      break;
+  }
+
+  return result;
+}
+
+(async function main() {
+  console.log(
+    `🏁 🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando...\n`
+  );
+  await playRaceEngine(player1, player2);
+})();
